da164ab00d30506731b4b55b803dd3ac7050b919
sami suliman <sami.bco@gmail.com>
2019-11-30 15:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+var newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = '<h1>victory</h1>';
+
+newHeader.innerText = 'YOUR-NAME is the champion';
